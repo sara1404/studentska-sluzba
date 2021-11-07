6d10ff35dbee0f9794127899024b5d2e15da3edf
Jelena Dinić <dinic.ra44.2019@uns.ac.rs>
2021-11-07 14:50:03 +0100

    main window created

---
+++ src/view/MainFrame.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainFrame extends JFrame {
+	
+	public MainFrame(){
+		setTitle("Studentska služba");
+		setVisible(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension dim = kit.getScreenSize();
+		int width = dim.width;
+		int height = dim.height;
+		setSize(width * 3/4, height *3/4);
+		setLocationRelativeTo(null);
+	}
+}
---
+++ src/view/MyApp.java
@@ -0,0 +1,10 @@
+package view;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		MainFrame mf = new MainFrame();
+		
+	}
+
+}

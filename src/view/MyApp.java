bb15a3df3217f61c8b79552d0097bf497b0d19a8
Jelena Dinić <dinic.ra44.2019@uns.ac.rs>
2021-11-16 15:48:01 +0100

    menu bar updated

--- src/view/MyApp.java
+++ src/view/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		MainFrame mf = new MainFrame();
-		AddStudentDialog asw = new AddStudentDialog();
+		AddStudentDialog asd = new AddStudentDialog();
 		AddProfessorDialog apd = new AddProfessorDialog();
 		
 	}

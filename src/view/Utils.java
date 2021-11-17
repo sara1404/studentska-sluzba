37d559d4e9fb65ec84592be3ca129bdc4d71b892
sara1404 <sinjeri.ra76.2019@uns.ac.rs>
2021-11-17 20:54:40 +0100

    windows updated

--- src/view/Utils.java
+++ src/view/Utils.java
@@ -2,10 +2,17 @@ package view;
 
 import javax.swing.JComponent;
 import java.awt.Cursor;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Utils {
 	
 	public static void setCursor(JComponent component) {
 		component.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+	}
+	
+	public static String DateToString(LocalDate date) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+		return date.format(formatter);
 	}
 }
--- src/view/WindowComponentBuilder.java
+++ src/view/WindowComponentBuilder.java
@@ -3,9 +3,13 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class WindowComponentBuilder {
@@ -35,5 +39,25 @@ public class WindowComponentBuilder {
 		comp.setMinimumSize(new Dimension(width, height));
 		comp.setMaximumSize(new Dimension(width, height));
 		comp.setPreferredSize(new Dimension(width, height));
+	}
+	
+	public static JPanel createButtons(JButton btn1, JButton btn2) {
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		//JButton apply = new JButton();
+		WindowComponentBuilder.setContainerSize(btn1, 100, 30);
+//		JButton cancel = new JButton();
+		WindowComponentBuilder.setContainerSize(btn2, 100, 30);
+		btn1.setText("Potvrdi");
+		btn2.setText("Odustani");
+		Utils.setCursor(btn1);
+		Utils.setCursor(btn2);
+		panel.add(btn1);
+		panel.add(Box.createHorizontalStrut(40));
+		panel.add(btn2);
+		BoxLayout box = new BoxLayout(panel, BoxLayout.X_AXIS);
+		panel.setLayout(box);
+		
+		return panel;
 	}
 }

12dfc1e37af787a8395b1f09124a59757c4b8fe2
Jelena Dinić <dinic.ra44.2019@uns.ac.rs>
2021-11-19 17:01:50 +0100

    add professor window modified

--- src/view/AddProfessorDialog.java
+++ src/view/AddProfessorDialog.java
@@ -21,7 +21,6 @@ public class AddProfessorDialog extends JDialog implements ActionListener{
 	
 	private void setPanel(JPanel pan) {
 		pan.setBackground(Color.DARK_GRAY);
-		pan.add(Box.createHorizontalStrut(200));
        
 	}
 	
@@ -48,81 +47,81 @@ public class AddProfessorDialog extends JDialog implements ActionListener{
 		
 		 //surname
         
-		JPanel panSurname = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel panSurname = new JPanel();
 		setPanel(panSurname);
 		panSurname.add(WindowComponentBuilder.createLabel("Prezime*"));
         panSurname.add(WindowComponentBuilder.createTextField());
         panCenter.add(panSurname);
         
 		//name
-		JPanel panName = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel panName = new JPanel();//(new FlowLayout(FlowLayout.LEFT));
 		setPanel(panName);
 		panName.add(WindowComponentBuilder.createLabel("Ime*"));
         panName.add(WindowComponentBuilder.createTextField());
         panCenter.add(panName);
         
         //date of birth
-      	JPanel panDate = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      	JPanel panDate = new JPanel();
       	setPanel(panDate);
       	panDate.add(WindowComponentBuilder.createLabel("Datum rodjenja*"));
         panDate.add(WindowComponentBuilder.createTextField());
         panCenter.add(panDate);
         
        //home adress
-        JPanel panHome = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panHome = new JPanel();
         setPanel(panHome);
         panHome.add(WindowComponentBuilder.createLabel("Adresa stanovanja*"));
         panHome.add(WindowComponentBuilder.createTextField());
         panCenter.add(panHome);
         
         //phone
-        JPanel panPhone = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panPhone = new JPanel();
         setPanel(panPhone);
         panPhone.add(WindowComponentBuilder.createLabel("Kontakt telefon*"));
         panPhone.add(WindowComponentBuilder.createTextField());
         panCenter.add(panPhone);
         
         //email
-        JPanel panEmail= new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panEmail= new JPanel();
         setPanel(panEmail);
         panEmail.add(WindowComponentBuilder.createLabel("E-mail*"));
         panEmail.add(WindowComponentBuilder.createTextField());
         panCenter.add(panEmail);
         
         //office adress
-        JPanel panOffice= new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panOffice= new JPanel();
         setPanel(panOffice);
         panOffice.add(WindowComponentBuilder.createLabel("Adresa kancelarije*"));
         panOffice.add(WindowComponentBuilder.createTextField());
         panCenter.add(panOffice);
         
         //id
-        JPanel panId= new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panId= new JPanel();
         setPanel(panId);
         panId.add(WindowComponentBuilder.createLabel("Broj licne karte*"));
         panId.add(WindowComponentBuilder.createTextField());
         panCenter.add(panId);
         
         //title
-        JPanel panTitle= new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panTitle= new JPanel();
         setPanel(panTitle);
         panTitle.add(WindowComponentBuilder.createLabel("Zvanje*"));
         panTitle.add(WindowComponentBuilder.createTextField());
         panCenter.add(panTitle);
         
         //years od service
-        JPanel panService= new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panService= new JPanel();
         setPanel(panService);
         panService.add(WindowComponentBuilder.createLabel("Godine staza*"));
         panService.add(WindowComponentBuilder.createTextField());
         panCenter.add(panService);
         
         //list of subjects
-        JPanel panSubjects = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        /*JPanel panSubjects = new JPanel();
         setPanel(panSubjects);
         panSubjects.add(WindowComponentBuilder.createLabel("Spisak predmeta*"));
         panSubjects.add(WindowComponentBuilder.createMultipleSelectionList());
-        panCenter.add(panSubjects);
+        panCenter.add(panSubjects);*/
         
       
         panCenter.add(Box.createVerticalStrut(25));  

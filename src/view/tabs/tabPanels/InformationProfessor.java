29456fb3a2ab0e8a60226100f0bd2639753b87ed
Jelena Dinić <dinic.ra44.2019@uns.ac.rs>
2021-12-07 23:08:16 +0100

    change professor modified

--- src/view/tabs/tabPanels/InformationProfessor.java
+++ src/view/tabs/tabPanels/InformationProfessor.java
@@ -2,17 +2,23 @@ package view.tabs.tabPanels;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import controller.ListenerController;
+import utils.Utils;
 import utils.WindowComponentBuilder;
 
 public class InformationProfessor extends JPanel{
 	
 	JPanel panCenter = new JPanel();
 	BoxLayout boxCenter = new BoxLayout(panCenter, BoxLayout.Y_AXIS);
+	ArrayList<JButton> buttons;
 	
 	private void setPanel(String str, int field) {
 		JPanel pan = new JPanel();
@@ -36,6 +42,7 @@ public class InformationProfessor extends JPanel{
 
 	public InformationProfessor() {
 		
+		buttons= new ArrayList<>();
 		panCenter.setLayout(boxCenter);
 		panCenter.setBackground(Color.DARK_GRAY);
 	
@@ -52,5 +59,34 @@ public class InformationProfessor extends JPanel{
         
         panCenter.add(Box.createVerticalStrut(25));  
         add(panCenter,BorderLayout.CENTER);
+        
+        JPanel panBottom=new JPanel();
+		BoxLayout box=new BoxLayout(panBottom, BoxLayout.X_AXIS);
+		panBottom.setLayout(box);
+		panBottom.setBackground(Color.DARK_GRAY);
+		
+		JButton btnOk=new JButton("Potvrdi");
+		btnOk.setPreferredSize(new Dimension(100,25));
+		
+		JButton btnCancel=new JButton("Odustani");
+		btnCancel.setPreferredSize(new Dimension(100,25));
+		
+		panBottom.add(Box.createHorizontalStrut(200));
+		panBottom.add(btnOk);
+		Utils.setCursor(btnOk);
+		panBottom.add(Box.createHorizontalStrut(100));
+		panBottom.add(btnCancel);
+		Utils.setCursor(btnCancel);
+		buttons.add(btnOk);
+		buttons.add(btnCancel);
+		panBottom.add(Box.createHorizontalStrut(10));
+		panBottom.add(Box.createVerticalStrut(50));
+		
+
+		add(panBottom,BorderLayout.SOUTH);
+	}
+	
+	public ArrayList<JButton> getButtons() {
+		return buttons;
 	}
 }
